c51d8134154f8d5080de3f8afeb1cca4b0ce7a2e
Aymeric Brisse <aymeric.brisse@gmail.com>
2014-06-12 01:19:02 +0200

    Problem 1880 - Psych Up's Eigenvalues

---
+++ 1880/script.rb
@@ -0,0 +1,13 @@
+# Problem 1880 - Psych Up's Eigenvalues
+# URL: http://acm.timus.ru/problem.aspx?space=1&num=1880
+#
+# Simple Array Intersection
+
+gets
+arr1 = gets.split(' ')
+gets
+arr2 = gets.split(' ')
+gets
+arr3 = gets.split(' ')
+
+puts (arr1 & arr2 & arr3).size
